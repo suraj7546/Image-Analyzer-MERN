27bfda1d7b8cf5c87b6beb8f6417214e85fcd913
suraj <surajkumar72501@gmail.com>
2023-08-02 05:04:59 +0530

    7th commit

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,33 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 import { NavLink } from "react-router-dom";
+import HamburgerMenu from "react-hamburger-menu";
 
-// Functional component 'Navbar' responsible for rendering the navigation bar
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleHamburgerClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       {/* Navigation bar */}
-      <nav className="main-nav-bar">
+      <nav className={`main-nav-bar ${isOpen ? "open" : ""}`}>
         <div className="list">
           <ul>
             {/* Home link */}
             <li>
-              <NavLink to="/">Home</NavLink>
+              <NavLink exact to="/" ClassName="active">
+                Home
+              </NavLink>
             </li>
 
             {/* Object recognition link */}
             <li>
-              <NavLink to="/about">Object recognition</NavLink>
+              <NavLink to="/about" ClassName="active">
+                Object recognition
+              </NavLink>
             </li>
 
             {/* Color link */}
             <li>
-              <NavLink to="/color">Color</NavLink>
+              <NavLink to="/color" ClassName="active">
+                Color
+              </NavLink>
             </li>
 
             {/* Safe Search link */}
             <li>
-              <NavLink to="/safe">Safe Search</NavLink>
+              <NavLink to="/safe" ClassName="active">
+                Safe Search
+              </NavLink>
             </li>
           </ul>
         </div>
